d100b921b59f08f58a7892a4c431a7558928c30f
Soundharrajan V <soundharrajan.v@ltts.com>
2023-03-09 15:26:19 +0530

    fix: ui/ux updated

--- dashboard-components/src-dashboard-component/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const App = () => (
-  <><div>
-    <div>Dashboard Components Applications</div>
-  </div></>
-);
-ReactDOM.render(<App />, document.getElementById("app"));
